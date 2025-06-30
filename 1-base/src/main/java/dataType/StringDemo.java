0a2d3e071cf5068c8265b5365bbf4a69af60afde
zhangyajun <zhang.yajun33@iwhalecloud.com>
2025-06-30 12:59:24 +0800

    init

---
+++ 1-base/src/main/java/dataType/StringDemo.java
@@ -0,0 +1,9 @@
+package dataType;
+
+/**
+ * @author: zh4ngyj
+ * @date: 2025/6/30 12:32
+ * @des:
+ */
+public class StringDemo {
+}
---
+++ 2-advance/1-collection/src/main/java/collections/Map.java
@@ -0,0 +1,9 @@
+package dataType;
+
+/**
+ * @author: zh4ngyj
+ * @date: 2025/6/30 12:35
+ * @des:
+ */
+public class Map {
+}
