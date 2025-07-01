07dd98d21c1c138458a5f5bfab4b1161530038d1
zhangyajun <zhang.yajun33@iwhalecloud.com>
2025-07-01 14:19:12 +0800

    init

---
+++ 13-designpattern/src/main/java/SingletonDemo.java
@@ -0,0 +1,13 @@
+// 单例模式：确保一个类只有一个实例
+// https://www.cnblogs.com/sun-10387834/p/18950201
+// 饿汉式、懒汉式、同步方法、双检锁式、静态内部类式、枚举式
+// 避免反射和序列化攻击
+
+/**
+ * @author: zh4ngyj
+ * @date: 2025/7/1 14:16
+ * @des:
+ */
+public class SingletonDemo {
+
+}
